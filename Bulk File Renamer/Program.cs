7608382a979d20d6ae99016404d2268205ae69a0
algmi <algmi@ALEKSEY_MSI>
2023-10-29 12:12:17 +0300

    Добавьте файлы проекта.

---
+++ Bulk File Renamer/Program.cs
@@ -0,0 +1,43 @@
+﻿using Bulk_File_Renamer;
+
+bool dirIsValid = false;
+string dir = string.Empty;
+
+while (!dirIsValid)
+{
+    Console.WriteLine("Введите адрес папки: ");
+    dir = Console.ReadLine();
+    if (Directory.Exists(dir)) 
+    {  
+        dirIsValid = true;
+    }
+}
+
+var files = Directory.GetFiles(dir);
+
+Console.WriteLine($"В папке {dir} содержится {files.Length} файлов");
+
+Console.WriteLine("Переименовать их? Y/N");
+var renameAnswer = Console.ReadLine();
+
+if (renameAnswer == "y" || renameAnswer == "Y")
+{
+    Renamer.Rename(dir);
+    Console.WriteLine("Все файлы переименованы успешно!\n Завершение работы....");
+    Thread.Sleep(2000);
+    Environment.Exit(0);
+}
+else
+{
+    {
+        Console.WriteLine("завершение работы....");
+        Thread.Sleep(2000);
+        Environment.Exit(0);
+    }
+}
+
+
+
+
+
+
---
+++ Bulk File Renamer/Renamer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulk_File_Renamer
+{
+    public class Renamer
+    {
+        public static void Rename(string path)
+        {
+            int num = 1;
+            var files = Directory.GetFiles(path).ToList();
+            string newFileName = string.Empty;
+
+            // int num = files.Count;
+            try { 
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string fileExtension = Path.GetExtension(file);
+                if (fileExtension != "null" && (fileExtension == ".jpg" || fileExtension == ".JPG"))
+                {
+                    if (num < 10)
+                    {
+                        newFileName = $"00000{num}{fileExtension}";
+                        string newFilePath = Path.Combine(path, newFileName);
+                        File.Move(file, newFilePath);
+                    }
+                    else if (num > 9 && num < 100)
+                    {
+                        newFileName = $"0000{num}{fileExtension}";
+                        string newFilePath = Path.Combine(path, newFileName);
+                        File.Move(file, newFilePath);
+                    }
+                    else if (num > 99 && num < 1000)
+                    {
+                        newFileName = $"000{num}{fileExtension}";
+                        string newFilePath = Path.Combine(path, newFileName);
+                        File.Move(file, newFilePath);
+                    }
+                    else if (num > 999 && num < 10000)
+                    {
+                        newFileName = $"00{num}{fileExtension}";
+                        string newFilePath = Path.Combine(path, newFileName);
+                        File.Move(file, newFilePath);
+                    }
+                    num++;
+                }
+
+            }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+
+            Console.WriteLine("Все файлы успешно переименованы.");
+        }
+    }
+}
